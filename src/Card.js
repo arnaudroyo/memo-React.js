1f267e45e4ec61eb4725ef2eb747848477562dfb
Arnaud.Royo <arnaud.royo@epsi.fr>
2020-03-06 15:27:50 +0100

    PropTypes

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import './Card.css'
 
@@ -12,4 +13,15 @@ const Card = ({ card, feedback, onClick }) => (
     </div>
   )
 
+
+  Card.propType = {
+    card: PropTypes.string.isRequired,
+    feedback: PropTypes.oneOf([
+      'visible',
+      'hidden',
+      'justMatched',
+      'justMismatched',
+    ]).isRequired,
+    onClick: PropTypes.func.isRequired
+  }
 export default Card
